a47eda3f752104434a758df8c36c5846fd85726a
Ramtin <31255934+rradfar@users.noreply.github.com>
2020-11-05 17:16:31 +0400

    Add toggle functionality to nav btn

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,7 @@
 const navToggleBtn = document.getElementById('nav-toggle-btn');
+const navMenu = document.getElementById('nav-menu');
 
-const toggleMenu = () => navToggleBtn.classList.toggle('open');
+const toggleMenu = () => {
+  navToggleBtn.classList.toggle('open');
+  navMenu.classList.toggle('open');
+}
